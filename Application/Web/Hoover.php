682f3d471184fba6ac4c50fb14921658f56e0cf3
HuangStomach <nxmbest@qq.com>
2019-03-16 22:18:06 +0800

    扫描网站的标签和属性值

--- Application/Web/Hoover.php
+++ Application/Web/Hoover.php
@@ -13,4 +13,39 @@ class Hoover {
         }
         return $this->content;
     }
+
+    public function getTags($url, $tag) {
+        $count = 0;
+        $result = [];
+        $elements = $this->getContent($url)->getElementsByTagName($tag);
+        foreach ($elements as $node) {
+            $result[$content]['value'] = trim(preg_replace('/\s+/', ' ', $node->nodeValue));
+            if ($node->hasAttributes()) {
+                foreach ($node->attributes as $name => $attr) {
+                    $result[$count]['attributes'][$name] = $attr->value;
+                }
+            }
+            $count++;
+        }
+        return $result;
+    }
+
+    public function getAttribute($url, $attr, $domain = NULL) {
+        $result = [];
+        $elements = $this->getContent($url)->getElementsByTagName('*');
+        foreach ($elements as $node) {
+            if ($node->hasAttributes($attr)) {
+                $value = $node->getAttribute($attr);
+                if ($domain) {
+                    if (stripos($value, $domain) !== FALSE) {
+                        $result[] = trim($value);
+                    }
+                }
+                else {
+                    $result[] = trim($value);
+                }
+            }
+        }
+        return $result;
+    }
 }
