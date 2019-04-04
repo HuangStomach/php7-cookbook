db93f06d69d26fb9257a0540b1bece654222ac13
HuangStomach <nxmbest@qq.com>
2019-04-04 21:23:59 +0800

    根据用户所在地显示适当的数字

--- Application/I18N/Locale.php
+++ Application/I18N/Locale.php
@@ -1,14 +1,21 @@
 <?php
 namespace Application\I18n;
 
+use NumberFormatter;
 use Locale as PhpLocale;
 
 class Locale extends PhpLocale {
     const FALLBACK_LOCALE = 'en';
     protected $localeCode;
+    protected $numberFormatter;
 
-    public function setLocaleCode($acceptLangHeader) {
-        $this->localeCode = $this->acceptFromHttp($acceptLangHeader);
+    public function __construct($localeString = null) {
+        if ($localeString) {
+            $this->setLocaleCode($localeString);
+        }
+        else {
+            $this->setLocaleCode($this->getAcceptLanguage());
+        }
     }
 
     public function getAcceptLanguage() {
@@ -19,12 +26,23 @@ class Locale extends PhpLocale {
         return $this->localeCode;
     }
 
-    public function __construct($localeString = null) {
-        if ($localeString) {
-            $this->setLocaleCode($localeString);
+    public function setLocaleCode($acceptLangHeader) {
+        $this->localeCode = $this->acceptFromHttp($acceptLangHeader);
+    }
+
+    public function getNumberFormatter() {
+        if (!$this->numberFormatter) {
+            $this->numberFormatter = new NumberFormatter($this->getLocaleCode(), NumberFormatter::DECIMAL);
         }
-        else {
-            $this->setLocaleCode($this->getAcceptLanguage());
-        }
+        return $this->numberFormatter;
+    }
+
+    public function formatNumber($number) {
+        return $this->getNumberFormatter()->format($number);
+    }
+
+    public function parseNumber($string) {
+        $result = $this->getNumberFormatter()->parse($string);
+        return ($result) ? $result : 'error';
     }
 }
