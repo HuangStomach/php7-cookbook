2f71bd829c8fd4a1796b4684ece1d47bc8aa10ee
HuangStomach <nxmbest@qq.com>
2019-04-15 21:12:51 +0800

    实现堆栈

---
+++ Application/Generic/Stack.php
@@ -0,0 +1,24 @@
+<?php
+namespace Application\Generic;
+
+use SplStack;
+
+class Stack {
+    protected $stack;
+
+    public function __construct() {
+        $this->stack = new SplStack();
+    }
+
+    public function push($message) {
+        $this->stack->push($message);
+    }
+
+    public function pop() {
+        $this->stack->pop();
+    }
+
+    public function __invoke() {
+        return $this->stack;
+    }
+}
