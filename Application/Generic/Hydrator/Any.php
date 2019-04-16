fb5fe6d8b9e2de403cf3ea6b0e729f5c75a0c220
HuangStomach <nxmbest@qq.com>
2019-04-16 21:11:53 +0800

    策略模式 策略选择器

---
+++ Application/Generic/Hydrator/Any.php
@@ -0,0 +1,62 @@
+<?php
+namespace Application\Generic\Hydrator;
+
+use InvalidArgumentException;
+use Application\Generic\Hydrator\Strategy\ { GetSet, PublicProps, Extending };
+
+class Any {
+    const STRATEGY_PUBLIC = 'PublicProps';
+    const STRATEGY_GET_SET = 'GetSet';
+    const STRATEGY_EXTEND = 'Extending';
+    protected $strategies;
+    public $chosen;
+
+    public function __construct() {
+        $this->strategies[self::STRATEGY_EXTEND] = new Extending();
+        $this->strategies[self::STRATEGY_GET_SET] = new GetSet();
+        $this->strategies[self::STRATEGY_PUBLIC] = new PublicProps();
+    }
+
+    public function addStrategy($key, HydratorInterface $strategy) {
+        $this->strategies[$key] = $strategy;
+    }
+
+    public function hydrate(array $array, $object) {
+        $strategy = $this->chooseStrategy($object);
+        $this->chosen = get_class($strategy);
+        return $strategy::hydrate($array, $object);
+    }
+
+    public function extract($object) {
+        $strategy = $this->chooseStrategy($object);
+        $this->chosen = get_class($strategy);
+        return $strategy::extract($object);
+    }
+
+    public function chooseStrategy($object) {
+        $strategy = null;
+        $methodList = get_class_methods(get_class($object));
+        if (!empty($methodList) && is_array($methodList)) {
+            $getset = false;
+            foreach ($methodList as $method) {
+                if (preg_match('/^get|set.*$/i', $method)) {
+                    $strategy = $this->strategies[self::STRATEGY_GET_SET];
+                    break;
+                }
+            }
+        }
+
+        if (!$strategy) {
+            $vars = get_class_vars(get_class($object));
+            if (!empty($vars) && count($vars)) {
+                $strategy = $this->strategies[self::STRATEGY_PUBLIC];
+            }
+        }
+
+        if (!$strategy) {
+            $strategy = $this->strategies[self::STRATEGY_EXTEND];
+        }
+
+        return $strategy;
+    }
+}
