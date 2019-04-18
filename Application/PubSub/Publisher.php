bdefd0502835e11be0f8d5e7925399515356c3e6
HuangStomach <nxmbest@qq.com>
2019-04-18 21:48:59 +0800

    订阅模式 发布者

---
+++ Application/PubSub/Publisher.php
@@ -0,0 +1,43 @@
+<?php
+namespace Application\PubSub;
+
+use SplSubject;
+use SplObserver;
+
+class Publisher implements SplSubject {
+    protected $name;
+    protected $data;
+    protected $linked;
+    protected $subscribers;
+
+    public function __construct($name) {
+        $this->name = $name;
+        $this->data = [];
+        $this->subscribers = [];
+        $this->linked = [];
+    }
+
+    public function __toString() {
+        return $this->name;
+    }
+
+    public function attach(SplObserver $subscriber) {
+        $this->subscribers[$subscriber->getKey()] = $subscriber;
+        $this->linked[$subscriber->getKey()] = $subscriber->getPriority();
+    }
+
+    public function detach(SplObserver $subscriber) {
+        unset($this->subscribers[$subscriber->getKey()]);
+        unset($this->linked[$subscriber->getKey()]);
+    }
+
+    public function notify() {
+        foreach ($this->linked as $key => $value) {
+            $this->subscribers[$key]->update($this);
+        }
+    }
+
+    public function setDataByKey($key, $value) {
+        $this->data[$key] = $value;
+    }
+}
