b87cd13f2f6bec03f728334c3bb452e8e899d5e2
HuangStomach <nxmbest@qq.com>
2019-04-19 22:15:02 +0800

    通用错误处理程序

--- Application/Error/Handler.php
+++ Application/Error/Handler.php
@@ -12,7 +12,13 @@ class Handler {
     }
 
     public function exceptionHandler($ex) {
-        $message = sprintf('%19s : %20s : %s' . PHP_EOL, date('Y-m-d H:i:s'), get_class($ex), $ex->getMessage());
+        $message = sprintf('EXCEPTION: %19s : %20s : %s' . PHP_EOL, date('Y-m-d H:i:s'), get_class($ex), $ex->getMessage());
+        file_put_contents($this->logFile, $message, FILE_APPEND);
+    }
+
+    public function errorHandler($errno, $errstr, $errfile, $errline) {
+        $message = sprintf('ERROR: %s : %d : %s : %s : %s' . PHP_EOL, date('Y-m-d H:i:s'), 
+            $errno, $errstr, $errfile, $errline);
         file_put_contents($this->logFile, $message, FILE_APPEND);
     }
 }
