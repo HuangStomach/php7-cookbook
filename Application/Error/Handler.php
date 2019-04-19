2058ea5ac22569faf563ecf826a761a52f3f9924
HuangStomach <nxmbest@qq.com>
2019-04-19 22:07:12 +0800

    异常处理程序

---
+++ Application/Error/Handler.php
@@ -0,0 +1,18 @@
+<?php
+namespace Application\Error;
+
+class Handler {
+    protected $logFile;
+    public function __construct($logFileDir = null, $logFile = null) {
+        $logFile = $logFile ?? date('Ymd') . '.log';
+        $logFileDir = $logFileDir ?? __DIR__;
+        $this->logFile = $logFileDir . '/' . $logFile;
+        $this->logFile = str_replace('//', '/', $this->logFile);
+        set_exception_handler([$this, 'exceptionHandler']);
+    }
+
+    public function exceptionHandler($ex) {
+        $message = sprintf('%19s : %20s : %s' . PHP_EOL, date('Y-m-d H:i:s'), get_class($ex), $ex->getMessage());
+        file_put_contents($this->logFile, $message, FILE_APPEND);
+    }
+}
